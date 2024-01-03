1857af885a3a72fd0bda7d72e73bdb2ecfad11c0
shahromsharipov <sharipov-shahrom@inbox.ru>
2024-01-03 01:21:56 -0600

    hello

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://technovaschool.github.io/TechNovaBank",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
